220d261ffdd6bf6f59ac11de7835944e96f5e5c8
Anartz Mugika <mugan86@gmail.com>
2018-02-05 18:57:52 +0100

    Add HTML extension to add html very easy in custom textview

--- app/src/main/java/mugan86/custom/fonts/example/MainActivity.kt
+++ app/src/main/java/mugan86/custom/fonts/example/MainActivity.kt
@@ -2,11 +2,14 @@ package mugan86.custom.fonts.example
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import mugan86.custom.font.textview.addHtml
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        customWithFontBold.addHtml("<u>Hola Anartz!!</u>")
     }
 }
---
+++ customfonttextview/src/main/java/mugan86/custom/font/textview/Extensions.kt
@@ -0,0 +1,15 @@
+package mugan86.custom.font.textview
+
+import android.os.Build
+import android.text.Html
+
+/********************************************************************************
+ * Created by Anartz Mugika (mugan86@gmail.com) on 5/2/18.
+ ********************************************************************************/
+fun CustomFontTextView.addHtml(html: String) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+        text = Html.fromHtml(html)
+    } else {
+        text = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT)
+    }
+}
