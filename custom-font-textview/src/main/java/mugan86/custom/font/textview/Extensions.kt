9e3a05062eb974be452bdc59d9be0fc5a60db184
Anartz Mugika <mugan86@gmail.com>
2018-02-07 20:04:20 +0100

    Add new function to concat text

--- custom-font-textview/src/main/java/mugan86/custom/font/textview/Extensions.kt
+++ custom-font-textview/src/main/java/mugan86/custom/font/textview/Extensions.kt
@@ -6,10 +6,10 @@ import android.text.Html
 /********************************************************************************
  * Created by Anartz Mugika (mugan86@gmail.com) on 5/2/18.
  ********************************************************************************/
-fun CustomFontTextView.addHtml(html: String) {
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-        text = Html.fromHtml(html)
-    } else {
-        text = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT)
-    }
+fun CustomFontTextView.addHtml(html: String) =
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) text = Html.fromHtml(html)
+                    else text = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT)
+
+fun CustomFontTextView.concat(addText: String){
+    text = "${text.toString()} $addText"
 }
