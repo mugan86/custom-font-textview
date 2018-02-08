4f20e9df08fd8071bcdae3901b743359767dcc7f
Anartz Mugika <mugan86@gmail.com>
2018-02-08 18:52:04 +0100

    Add custom fonts function Select Random font from select list

--- custom-font-textview/src/main/java/mugan86/custom/font/textview/Extensions.kt
+++ custom-font-textview/src/main/java/mugan86/custom/font/textview/Extensions.kt
@@ -1,7 +1,10 @@
 package mugan86.custom.font.textview
 
+import android.content.Context
+import android.graphics.Typeface
 import android.os.Build
 import android.text.Html
+import java.util.*
 
 /********************************************************************************
  * Created by Anartz Mugika (mugan86@gmail.com) on 5/2/18.
@@ -13,4 +16,28 @@ fun CustomFontTextView.addHtml(html: String) =
 fun CustomFontTextView.concat(addText: String) {
     text = text.toString().plus(addText)
 }
+
+fun CustomFontTextView.loadCustomFont(customFont: String, context: Context) {
+    typeface = Typeface.createFromAsset(
+            context.assets,
+            "font/$customFont"
+    )
+}
+
+fun selectRandomFont(context: Context, arrayStringXMLReference: Int = R.array.defaults_fonts): String =
+        context.resources.getStringArray(arrayStringXMLReference)
+            .get(takeOneFontWithRandom(1, 0,
+                    context.resources.getStringArray(arrayStringXMLReference).size - 1)[0])
+
+// Random
+
+private fun takeOneFontWithRandom(numbersTotal: Int = 1, min: Int, max: Int) : List<Int> {
+    val list = List(numbersTotal) { randomNumber(min, max + 1) }.distinct()
+    if (list.size == numbersTotal) return list
+    return takeOneFontWithRandom(numbersTotal, min, max)
+}
+
+private fun randomNumber(min: Int, max: Int): Int = (min..max).random()
+
+private fun ClosedRange<Int>.random() = Random().nextInt(endInclusive - start) +  start
 
